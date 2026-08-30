ddf9f6e6abb3b66fc15a6ec64f1e24fa379432ec
Abey Mathews <abeymathews@Abeys-MacBook-Pro.local>
2026-08-30 01:15:46 -0700

    feat(dev-agent): update studentApplicationForm.js

---
+++ force-app/main/default/lwc/studentApplicationForm-meta.xml/studentApplicationForm-meta.xml.js
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<LightningComponentBundle xmlns="http://soap.sforce.com/2006/04/metadata">
+    <apiVersion>59.0</apiVersion>
+    <isExposed>true</isExposed>
+    <masterLabel>Student Tuition Payment Portal Form</masterLabel>
+    <description>LWC form for student registration and tuition payment processing.</description>
+    <targets>
+        <target>lightning__AppPage</target>
+        <target>lightning__RecordPage</target>
+        <target>lightning__HomePage</target>
+    </targets>
+</LightningComponentBundle>
